94975b3e0e23f4a4af08445150f26359a2b87947
WalterMateo <sniperretlawusac@gmail.com>
2015-03-17 22:46:27 -0600

    4

--- src/Practica1.java
+++ src/Practica1.java
@@ -14,6 +14,8 @@ public class Practica1 {
 		int numero8=0;
 		int hora=0;
 		int minutos=0;
+		int numero9=0;
+		
 		System.out.println("Elija el Nivel de los problemas que desea ver 1 o 2");
 numero1 = s.nextInt();
 if(numero1 == 1){
@@ -58,17 +60,84 @@ if(numero1 == 1){
 	System.out.println("Ingrese la hora en notacion de 24 horas: ");
 	hora = s.nextInt();
 	System.out.println("Ingrese los minutos");
-	if(hora>=1 && hora<=12){
-		System.out.println(hora+minutos+"AM");
+	minutos = s.nextInt();
+	if(hora>=1 && hora<=12 && minutos<=60 && minutos>=0){
+		switch(hora){
+	case 01: System.out.println("01"+":"+minutos+" AM");
+	break;
+	case 02: System.out.println("02"+":"+minutos+" AM");
+	break;
+	case 03: System.out.println("03"+":"+minutos+" AM");
+	break;
+	case 04: System.out.println("04"+":"+minutos+" AM");
+	break;
+	case 05: System.out.println("05"+":"+minutos+" AM");
+	break;
+	case 06: System.out.println("06"+":"+minutos+" AM");
+	break;
+	case 07: System.out.println("07"+":"+minutos+" AM");
+	break;
+	case 8: System.out.println("08"+":"+minutos+" AM");
+	break;
+	case 9: System.out.println("09"+":"+minutos+" AM");
+	break;
+	case 10: System.out.println("10"+":"+minutos+" AM");
+	break;
+	case 11: System.out.println("11"+":"+minutos+" AM");
+	break;
+	case 12: System.out.println("12"+":"+minutos+" AM");
+	break;
+		}
 	}
-	else if(hora>12 && hora<=24){
-		
+	else if(hora>12 && hora<=24 && minutos<=60 && minutos>=0){
+		switch(hora){
+		case 13: System.out.println("01"+":"+minutos+" PM");
+		break;
+		case 14: System.out.println("02"+":"+minutos+" PM");
+		break;
+		case 15: System.out.println("03"+":"+minutos+" PM");
+		break;
+		case 16: System.out.println("04"+":"+minutos+" PM");
+		break;
+		case 17: System.out.println("05"+":"+minutos+" PM");
+		break;
+		case 18: System.out.println("06"+":"+minutos+" PM");
+		break;
+		case 19: System.out.println("07"+":"+minutos+" PM");
+		break;
+		case 20: System.out.println("08"+":"+minutos+" PM");
+		break;
+		case 21: System.out.println("09"+":"+minutos+" PM");
+		break;
+		case 22: System.out.println("10"+":"+minutos+" PM");
+		break;
+		case 23: System.out.println("11"+":"+minutos+" PM");
+		break;
+		case 24: System.out.println("12"+":"+minutos+" PM");
+		break;
+		}
 		
 	}
+	else
+		System.out.println("Los numero ingresados son invalidos");
+	
 }
+	else if(numero2==4){
+		System.out.println("Ingrese C, U, T");
+		String letra =s.nextLine(); 
+		if(letra==c){
+			
+		}
+		
+	}
 	}
 else if(numero1==2)
 System.out.println("Escoja un numero del 6 al 15 ");
+	numero9= s.nextInt();
+if(numero9==6){
+	System.out.println("ingrese dos numeros y un caracter");
+}
+	
 
 else
 	System.out.println("El numero no es 1 o 2");
